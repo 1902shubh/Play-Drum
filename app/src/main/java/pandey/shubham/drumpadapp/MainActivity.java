296fd6f5fb8ed359cf626cbb23250cc2a4f9063d
Shubham Pandey <shubhampandey979271@gmail.com>
2019-11-16 08:39:56 +0530

    Enjoy Drum

---
+++ app/src/main/java/pandey/shubham/drumpadapp/MainActivity.java
@@ -0,0 +1,79 @@
+package pandey.shubham.drumpadapp;
+
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer m1,m2,m3,m4,m5,m6,m7,m8;
+    @Override
+    public void onBackPressed() {
+        finish();
+        System.exit(0);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        m1=MediaPlayer.create(this,R.raw.one);
+        m2=MediaPlayer.create(this,R.raw.two);
+        m3=MediaPlayer.create(this,R.raw.three);
+        m4=MediaPlayer.create(this,R.raw.four);
+        m5=MediaPlayer.create(this,R.raw.fv);
+        m6=MediaPlayer.create(this,R.raw.sixth);
+        m7=MediaPlayer.create(this,R.raw.seventh);
+        m8=MediaPlayer.create(this,R.raw.eighth);
+
+    }
+    public void playSound(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.imageButton1:
+                m1.start();
+                break;
+            case R.id.imageButton2:
+                m2.start();
+                break;
+            case R.id.imageButton3:
+                m3.start();
+                break;
+            case R.id.imageButton4:
+                m4.start();
+                break;
+            case R.id.imageButton5:
+                m5.start();
+                break;
+            case R.id.imageButton6:
+                m6.start();
+                break;
+            case R.id.imageButton7:
+                m7.start();
+                break;
+            case R.id.imageButton8:
+                m8.start();
+                break;
+            case R.id.imageButton9:
+                m2.start();
+                break;
+            case R.id.imageButton10:
+                m5.start();
+                break;
+            case R.id.imageButton11:
+                m8.start();
+                break;
+            case R.id.imageButton12:
+                m3.start();
+                break;
+        }
+    }
+}
